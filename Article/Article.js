e84c7e8b037e26a1b661c357ace5f81df72dc982
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-06-11 22:08:37 -0500

    added animation for articles

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    // this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
@@ -15,6 +15,8 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-close');
+    this.expandButton.classList.toggle('clickToClose');
   }
 }
 
